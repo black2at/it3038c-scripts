5ec27324c778335f36f7865af849bb9232438ac6
black2at <black2at@mail.uc.edu>
2022-10-16 19:16:04 -0400

    fix lab 7

--- labs/Lab7.py
+++ labs/Lab7.py
@@ -4,7 +4,7 @@ print('please input a song file location')
 
 location = input()
 
-tag = TinyTag.get("C:/Users/airle/Music/American Idiot/Green Day - American Idiot.flac")
+tag = TinyTag.get(location)
 print('This track is by %s' % tag.artist)
 print('The track is in the album %s' % tag.album)
 print('The track is number %s' % tag.track )
