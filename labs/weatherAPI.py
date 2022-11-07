3649c5c872edec446629f6bdda26327f88e08787
black2at <112338576+black2at@users.noreply.github.com>
2022-11-06 19:34:09 -0500

    Delete weatherAPI.py

--- labs/weatherAPI.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-
-print('Please enter your zip code:')
-zip = input()
-
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=%s,us&appid=f95b1eff1c38bb0bbc41e1902fcfa686' % zip)
-data=r.json()
-print(data['weather'][0])
